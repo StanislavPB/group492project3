fe2099eef142cd02920b4e377f88a6136f13cf89
Oleksandr-Firko <152872222+Oleksandr-Firko@users.noreply.github.com>
2024-09-10 19:03:12 +0200

    added startUserInterface(), and some metod in for menu

--- src/main/java/org/group492project3/frontEnd/API/Api.java
+++ src/main/java/org/group492project3/frontEnd/API/Api.java
@@ -1,4 +1,65 @@
 package org.group492project3.frontEnd.API;
 
+import org.group492project3.backEnd.dto.Response;
+
 public class Api {
+
+
+    public Response<User, String> authorisation(String login, String password) {
+        //new User(login, password);
+        return new Response<>(null, false, null); //plug
+    }
+
+    public Response<NewUser, String> registration(String login, String password, String firstName, String secondName) {
+       // new NewUser(login, password, firstName, secondName);
+        return new Response<>(null, false, null); //plug
+    }
+
+    public static class User {
+        private String login;
+        private String password;
+
+        public User(String login, String password) {
+            this.login = login;
+            this.password = password;
+        }
+
+        public String getLogin() {
+            return login;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+    }
+
+    public static class NewUser {
+        private String login;
+        private String password;
+        private String firstName;
+        private String secondName;
+
+        public NewUser(String login, String password, String firstName, String secondName) {
+            this.login = login;
+            this.password = password;
+            this.firstName = firstName;
+            this.secondName = secondName;
+        }
+
+        public String getLogin() {
+            return login;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getSecondName() {
+            return secondName;
+        }
+    }
 }
--- src/main/java/org/group492project3/frontEnd/userMenu/UserMenu.java
+++ src/main/java/org/group492project3/frontEnd/userMenu/UserMenu.java
@@ -1,4 +1,76 @@
 package org.group492project3.frontEnd.userMenu;
 
+import org.group492project3.backEnd.dto.Response;
+import org.group492project3.frontEnd.API.Api;
+import org.group492project3.frontEnd.services.UserInputService;
+
 public class UserMenu {
+    private UserInputService userInput = new UserInputService();
+    private Api api = new Api();
+    //private MessageService message = new MessageService;
+
+    public UserMenu() {
+        startUserInterface();
+    }
+
+    private void startUserInterface() {
+        switch (userInput.getInt(
+                "Enter number of item to continue:\n" +
+                        "1.Login.\n" +
+                        "2.Registration.\n" +
+                        "3.Enter like Admin." +
+                        "Enter number:"
+        )) {
+            case 1: {
+                loginMenu();
+                break;
+            }
+            case 2: {
+                registrationMenu();
+                break;
+            }
+            case 3: {
+                adminMenu();
+                break;
+            }
+            default: {
+                //message.showError();
+                startUserInterface();
+            }
+        }
+    }
+
+    private void loginMenu() {
+        Response<Api.User, String> loginResult = api.authorisation(userInput.getString("Enter login:"), userInput.getString("Enter password:"));
+        if (loginResult.getStatusOfOperation()) {
+            //TODO: goToMainUserMenu();
+        } else {
+            //TODO: message.error(loginResult.getDescription());
+            startUserInterface();
+        }
+    }
+
+    private void registrationMenu() {
+        Response<Api.NewUser, String> registrationResult = api.registration(
+                userInput.getString("Enter login:"),
+                userInput.getString("Enter password:"),
+                userInput.getString("Enter first name:"),
+                userInput.getString("Enter second name:"));
+        if (registrationResult.getStatusOfOperation()) {
+            //TODO: goToMainUserMenu();
+        } else {
+            //TODO: message.error(loginResult.getDescription());
+            startUserInterface();
+        }
+    }
+
+    private void adminMenu() {
+        Response<Api.User, String> loginResult = api.authorisation(userInput.getString("Enter login:"), userInput.getString("Enter password:"));
+        if (loginResult.getStatusOfOperation()) {
+            //TODO: goToAdminMenu();
+        } else {
+            //TODO: message.error(loginResult.getDescription());
+            startUserInterface();
+        }
+    }
 }
