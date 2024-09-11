6d3ee95e24c3fde1c56fbc86a52c57719c01b575
Любовь <luba.kamyrina@gmail.com>
2024-09-11 11:53:02 +0200

    add two classes - EducationalMaterials and EdMatRepository.

--- src/main/java/org/group492project3/backEnd/entity/EducationalMaterials.java
+++ src/main/java/org/group492project3/backEnd/entity/EducationalMaterials.java
@@ -1,4 +1,55 @@
 package org.group492project3.backEnd.entity;
 
 public class EducationalMaterials {
+
+    private Integer idOfMaterial;
+    private Integer idOfCourse;
+    private String materialType;
+    private String materialDescription;
+
+
+    public Integer getIdOfCourse() {
+        return idOfCourse;
+    }
+
+    public Integer getIdOfMaterial() {
+        return idOfMaterial;
+    }
+
+    public String getMaterialDescription() {
+        return materialDescription;
+    }
+
+    public String getMaterialType() {
+        return materialType;
+    }
+
+    public void setIdOfMaterial(Integer idOfMaterial) {
+        this.idOfMaterial = idOfMaterial;
+    }
+
+    public void setMaterialDescription(String materialDescription) {
+        this.materialDescription = materialDescription;
+    }
+
+    public void setMaterialType(String materialType) {
+        this.materialType = materialType;
+    }
+
+    public EducationalMaterials(Integer idOfCourse, String materialType, String materialDescription) {
+        this.idOfCourse = idOfCourse;
+        this.idOfMaterial = idOfMaterial;
+        this.materialDescription = materialDescription;
+        this.materialType = materialType;
+    }
+
+    @Override
+    public String toString() {
+        return "EducationalMaterials{" +
+                "idOfCourse=" + idOfCourse +
+                ", idOfMaterial=" + idOfMaterial +
+                ", materialType='" + materialType + '\'' +
+                ", materialDescription='" + materialDescription + '\'' +
+                '}';
+    }
 }
---
+++ src/main/java/org/group492project3/backEnd/repository/EdMatRepository.java
@@ -0,0 +1,46 @@
+package org.group492project3.backEnd.repository;
+import org.group492project3.backEnd.entity.EducationalMaterials;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EdMatRepository {
+
+    private List<EducationalMaterials> materials = new ArrayList<>();
+    private Integer idMaterials = 0;
+
+    public EducationalMaterials addMaterial(Integer idOfCourse, String materialType, String materialDescription) {
+        idMaterials++;
+        EducationalMaterials materialsForSave = new EducationalMaterials(idOfCourse, materialType, materialDescription);
+        materials.add(materialsForSave);
+        return materialsForSave;
+    }
+
+    public List<EducationalMaterials> findAllMaterials() {
+        return materials;
+    }
+
+    public EducationalMaterials findByMaterialsId(Integer idOfMaterial) {
+
+        return materials.stream()
+                .filter(note -> note.getIdOfMaterial().equals(idOfMaterial))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public EducationalMaterials updateDescription(EducationalMaterials materialForUpdate) {
+        materialForUpdate.setMaterialDescription(materialForUpdate.getMaterialDescription());
+        return materialForUpdate;
+    }
+
+    public EducationalMaterials deleteMaterial(EducationalMaterials materialForDelete) {
+
+        for (EducationalMaterials currentItem : materials) {
+            if (currentItem.equals(materialForDelete)) {
+                materials.remove(materialForDelete);
+                System.out.println("Материал успешно удалён.");
+            }
+        } return null;
+
+    }
+}
